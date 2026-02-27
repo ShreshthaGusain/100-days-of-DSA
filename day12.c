59448a34db16e587ff7bc2d80efbb6ef0ddaed39
Shreshtha Gusain <shreshthagusain@gmail.com>
2026-02-27 23:33:55 +0530

    Create day12.c

--- day12.c
+++ day12.c
@@ -1,5 +1,4 @@
 // Day 12 - Question 1: Check Symmetric Matrix
-// Close
 // Problem: Write a program to check whether a given matrix is symmetric. 
 //A matrix is said to be symmetric if it is a square matrix and is equal to its transpose 
 //(i.e., element at position [i][j] is equal to element at position [j][i] for all valid i and j).
